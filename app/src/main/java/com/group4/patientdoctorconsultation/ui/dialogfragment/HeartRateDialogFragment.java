f9b4f09341d9770de715ad39d83a8b4f4b0a6708
Campbell Laptop <Campbell.M.Gorman@student.uts.edu.au>
2018-09-01 15:46:08 +1000

    Added ability to record heart rate and save to data packet

--- app/src/main/java/com/group4/patientdoctorconsultation/ui/dialogfragment/HeartRateDialogFragment.java
+++ app/src/main/java/com/group4/patientdoctorconsultation/ui/dialogfragment/HeartRateDialogFragment.java
@@ -1,39 +1,257 @@
 package com.group4.patientdoctorconsultation.ui.dialogfragment;
 
 import android.annotation.SuppressLint;
+import android.hardware.Camera;
+import android.os.Handler;
+import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
 import android.view.View;
-import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.group4.patientdoctorconsultation.R;
 import com.group4.patientdoctorconsultation.common.PacketItemDialog;
 import com.group4.patientdoctorconsultation.data.model.DataPacketItem;
+import com.group4.patientdoctorconsultation.utilities.ImageProcessor;
+
+import java.util.concurrent.atomic.AtomicBoolean;
 
 public class HeartRateDialogFragment extends PacketItemDialog {
 
-    EditText inputText;
+    private static final String TAG = HeartRateDialogFragment.class.getSimpleName();
+    private static final AtomicBoolean processing = new AtomicBoolean(false);
+
+    private static SurfaceHolder previewHolder;
+    private static Camera camera;
+    private static TextView text;
+    private static String heartRate;
+    private static ImageView heartImage;
+
+    private static int averageIndex = 0;
+    private static final int averageArraySize = 4;
+    private static final int[] averageArray = new int[averageArraySize];
+
+    public enum TYPE {
+        GREEN, RED
+    }
+
+    private static TYPE currentType = TYPE.GREEN;
+
+    public static TYPE getCurrent() {
+        return currentType;
+    }
+
+    private static int beatsIndex = 0;
+    private static final int beatsArraySize = 3;
+    private static final int[] beatsArray = new int[beatsArraySize];
+    private static double beats = 0;
+    private static long startTime = 0;
+
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        camera = Camera.open();
+        startTime = System.currentTimeMillis();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+
+        camera.setPreviewCallback(null);
+        camera.stopPreview();
+        camera.release();
+        camera = null;
+    }
+
+    private static Camera.PreviewCallback previewCallback = new Camera.PreviewCallback() {
+
+        @Override
+        public void onPreviewFrame(byte[] data, Camera cam) {
+            if (data == null) throw new NullPointerException();
+            Camera.Size size = cam.getParameters().getPreviewSize();
+            if (size == null) throw new NullPointerException();
+
+            if (!processing.compareAndSet(false, true)) return;
+
+            int width = size.width;
+            int height = size.height;
+
+            int imgAvg = ImageProcessor.decodeYUV420SPtoRedAvg(data.clone(), width, height);
+            // Log.i(TAG, "imgAvg="+imgAvg);
+            if (imgAvg == 0 || imgAvg == 255) {
+                processing.set(false);
+                return;
+            }
+
+            int averageArrayAvg = 0;
+            int averageArrayCnt = 0;
+            for (int anAverageArray : averageArray) {
+                if (anAverageArray > 0) {
+                    averageArrayAvg += anAverageArray;
+                    averageArrayCnt++;
+                }
+            }
+
+            int rollingAverage = (averageArrayCnt > 0) ? (averageArrayAvg / averageArrayCnt) : 0;
+            TYPE newType = currentType;
+            if (imgAvg < rollingAverage) {
+                newType = TYPE.RED;
+                if (newType != currentType) {
+                    beats++;
+                    heartImage.setVisibility(View.VISIBLE);
+                    Handler handler = new Handler();
+                    handler.postDelayed(new Runnable() {
+                        @Override
+                        public void run() {
+                            heartImage.setVisibility(View.GONE);
+                        }
+                    }, 100);
+                }
+            } else if (imgAvg > rollingAverage) {
+                newType = TYPE.GREEN;
+            }
+
+            if (averageIndex == averageArraySize) averageIndex = 0;
+            averageArray[averageIndex] = imgAvg;
+            averageIndex++;
+
+            // Transitioned from one state to another to the same
+            if (newType != currentType) {
+                currentType = newType;
+            }
+
+            long endTime = System.currentTimeMillis();
+            double totalTimeInSecs = (endTime - startTime) / 1000d;
+            if (totalTimeInSecs >= 10) {
+                double bps = (beats / totalTimeInSecs);
+                int dpm = (int) (bps * 60d);
+                if (dpm < 30 || dpm > 180) {
+                    startTime = System.currentTimeMillis();
+                    beats = 0;
+                    processing.set(false);
+                    return;
+                }
+
+                // Log.d(TAG,
+                // "totalTimeInSecs="+totalTimeInSecs+" beats="+beats);
+
+                if (beatsIndex == beatsArraySize) beatsIndex = 0;
+                beatsArray[beatsIndex] = dpm;
+                beatsIndex++;
+
+                int beatsArrayAvg = 0;
+                int beatsArrayCnt = 0;
+                for (int i = 0; i < beatsArray.length; i++) {
+                    if (beatsArray[i] > 0) {
+                        beatsArrayAvg += beatsArray[i];
+                        beatsArrayCnt++;
+                    }
+                }
+                int beatsAvg = (beatsArrayAvg / beatsArrayCnt);
+                heartRate = String.valueOf(beatsAvg) + " BPM";
+                text.setText("Current Heart Rate: " + heartRate);
+                startTime = System.currentTimeMillis();
+                beats = 0;
+            }
+            processing.set(false);
+        }
+    };
+
+    private static SurfaceHolder.Callback surfaceCallback = new SurfaceHolder.Callback() {
+
+        /**
+         * {@inheritDoc}
+         */
+        @Override
+        public void surfaceCreated(SurfaceHolder holder) {
+            try {
+                camera.setPreviewDisplay(previewHolder);
+                camera.setPreviewCallback(previewCallback);
+            } catch (Throwable t) {
+                Log.e("SURFACE_CALLBACK", "Exception in setPreviewDisplay()", t);
+            }
+        }
+
+        /**
+         * {@inheritDoc}
+         */
+        @Override
+        public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+            Camera.Parameters parameters = camera.getParameters();
+            parameters.setFlashMode(Camera.Parameters.FLASH_MODE_TORCH);
+            Camera.Size size = getSmallestPreviewSize(width, height, parameters);
+            if (size != null) {
+                parameters.setPreviewSize(size.width, size.height);
+                Log.d(TAG, "Using width=" + size.width + " height=" + size.height);
+            }
+            camera.setParameters(parameters);
+            camera.startPreview();
+        }
+
+        /**
+         * {@inheritDoc}
+         */
+        @Override
+        public void surfaceDestroyed(SurfaceHolder holder) {
+            // Ignore
+        }
+    };
+
+    private static Camera.Size getSmallestPreviewSize(int width, int height, Camera.Parameters parameters) {
+        Camera.Size result = null;
+
+        for (Camera.Size size : parameters.getSupportedPreviewSizes()) {
+            if (size.width <= width && size.height <= height) {
+                if (result == null) {
+                    result = size;
+                } else {
+                    int resultArea = result.width * result.height;
+                    int newArea = size.width * size.height;
+
+                    if (newArea < resultArea) result = size;
+                }
+            }
+        }
+
+        return result;
+    }
+
+    /////////////////////////////
 
     @Override
     public String getDialogResult(){
-        if(inputText != null){
-            return inputText.getText().toString();
-        }else{
-            return "";
-        }
+        return heartRate != null ? heartRate : "";
     }
 
     @Override
     public View getView(LayoutInflater inflater) {
+        @SuppressLint("InflateParams") View view = inflater.inflate(R.layout.fragment_dialog_heart_rate, null);
+        text = view.findViewById(R.id.text);
+        heartImage = view.findViewById(R.id.heart_icon);
 
-        @SuppressLint("InflateParams") View view = inflater.inflate(R.layout.fragment_dialog_text, null);
-        inputText = view.findViewById(R.id.text_input);
+        SurfaceView preview = view.findViewById(R.id.preview);
+        previewHolder = preview.getHolder();
+        previewHolder.addCallback(surfaceCallback);
 
         return view;
     }
 
     @Override
     public DataPacketItem.DataPacketItemType getPacketItemType() {
-        return DataPacketItem.DataPacketItemType.NOTE;
+        return DataPacketItem.DataPacketItemType.HEART_RATE;
     }
 
+    @Override
+    protected String getTitle() {
+        return "Record heart rate";
+    }
+
+    @Override
+    protected boolean saveEnabledByDefault() {
+        return true;
+    }
 }
